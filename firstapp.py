f953a04a4861cff2f54d9b6c8959f79fe3494b76
Yash-Git-Hub <69065839+Yash-Git-Hub@users.noreply.github.com>
2020-09-09 00:18:55 +0530

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -1,28 +1,25 @@
 import streamlit as st
+st.title("Lets find out how healthy you are : ")
 
+height = float( st.text_input("Enter Height in Centimeter ",0))
+weight = float(st.text_input("Enter Weight   in Kg  ",0))
+height=height/100
 
-height = float( st.text_input("Enter height in cm ", 1))
-weight = float(st.text_input("Enter hwight  in kg ", 1))
-
-# the formula for calculating bmi
-
-bmi = weight/(height**2) 
 # ** is the power of operator i.e height*height in this case
+if(weight>0):
+    bmi = weight/(height**2) 
+    st.write("## Your BMI is: {0:.2f} and you are: ".format(bmi), end='')
+    if ( bmi < 16):
+        st.write("## severely underweight")
 
-st.write("Your BMI is: {0} and you are: ".format(bmi), end='')
+    elif ( bmi >= 16 and bmi < 18.5):
+        st.write("## underweight")
 
-#conditions
-if ( bmi < 16):
-   st.write("severely underweight")
+    elif ( bmi >= 18.5 and bmi < 25):
+        st.write("## Healthy")
 
-elif ( bmi >= 16 and bmi < 18.5):
-   st.write("underweight")
+    elif ( bmi >= 25 and bmi < 30):
+        st.write("## overweight")
 
-elif ( bmi >= 18.5 and bmi < 25):
-   st.write("Healthy")
-
-elif ( bmi >= 25 and bmi < 30):
-   st.write("overweight")
-
-elif ( bmi >=30):
-   st.write("severely overweight")
+    elif ( bmi >=30):
+        st.write("## severely overweight")
